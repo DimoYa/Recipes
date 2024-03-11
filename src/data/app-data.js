2f93e8c534314dae2108692ffddf41b3f7a398e3
Dimo Yanev <dyanev@kpmg.com>
2024-03-11 16:41:21 +0200

    add app

---
+++ src/data/app-data.js
@@ -0,0 +1,108 @@
+let recipes = [];
+
+function seedSampleData() {
+  recipes.length = 0;
+  recipes.push({
+    id: 1,
+    name: "Spaghetti Aglio e Olio",
+    ingredients: "200g spaghetti,\n4 cloves of garlic,\n1/4 cup extra virgin olive oil,\n1/2 teaspoon red pepper flakes,\nSalt,\nFreshly ground black pepper,\nGrated Parmesan cheese,\nChopped fresh parsley",
+    description: "A classic Italian pasta dish featuring spaghetti tossed in garlic-infused olive oil with a hint of red pepper flakes.",
+    complexity: "1",
+    imageUrl: "https://theplantbasedschool.com/wp-content/uploads/2020/12/Spaghetti-4-1157x1536.jpg"
+  });
+  recipes.push({
+    id: 2,
+    name: "Chicken Parmesan",
+    ingredients: "2 boneless, skinless chicken breasts,\n1 cup breadcrumbs,\n1/2 cup grated Parmesan cheese,\n1 egg,\n1 cup marinara sauce,\n1 cup shredded mozzarella cheese,\nSalt,\nFreshly ground black pepper,\nChopped fresh basil",
+    description: "A classic Italian-American dish featuring breaded chicken breasts topped with marinara sauce and melted mozzarella cheese.",
+    complexity: "3",
+    imageUrl: "https://tastesbetterfromscratch.com/wp-content/uploads/2023/03/Chicken-Parmesan-1.jpg"
+  });
+  recipes.push({
+    id: 3,
+    name: "Caprese Salad",
+    ingredients: "2 large tomatoes,\n1 ball fresh mozzarella cheese,\n1/4 cup fresh basil leaves,\n2 tablespoons extra virgin olive oil,\n1 tablespoon balsamic vinegar,\nSalt,\nFreshly ground black pepper",
+    description: "A refreshing Italian salad featuring ripe tomatoes, creamy mozzarella, and fresh basil, drizzled with olive oil and balsamic vinegar.",
+    complexity: "1",
+    imageUrl: "https://whatsgabycooking.com/wp-content/uploads/2023/06/Dinner-Party-__-Traditional-Caprese-1.jpg"
+  });
+}
+
+function getRecipes() {
+  return recipes;
+}
+
+function findRecipeById(id) {
+  let resultRecipes = recipes.filter(recipe => recipe.id == id);
+  if (resultRecipes.length == 1)
+    return resultRecipes[0];
+  else
+    return { errMsg: `Cannot find recipe by id: ${id}` }
+}
+
+function findRecipesByKeyword(keyword) {
+  if (isParamEmpty(keyword))
+    return { errMsg: "Keyword cannot be empty!" };
+  keyword = keyword.toLowerCase();
+  let resultRecipes = recipes.filter(recipe =>
+    JSON.stringify(recipe).toLowerCase().includes(keyword)
+  );
+  return resultRecipes;
+}
+
+function addRecipe(name, complexity, ingredients, description, imageUrl) {
+  if (isParamEmpty(name))
+    return { errMsg: "Name cannot be empty!" };
+  if (isParamEmpty(description))
+    return { errMsg: "Description cannot be empty!" };
+  if (isParamEmpty(complexity))
+    return { errMsg: "Complexity cannot be empty!" };
+  if (!isValidImageUrl(imageUrl))
+    return { errMsg: "Invalid Image URL!" };
+  let newId = 1;
+  if (recipes.length > 0)
+    newId = 1 + recipes[recipes.length - 1].id;
+  let newRecipe = {
+    id: newId,
+    name: name,
+    ingredients: ingredients,
+    description: description,
+    complexity: complexity,
+    imageUrl: imageUrl,
+  };
+  recipes.push(newRecipe);
+  return { msg: "Recipe added.", recipe: newRecipe };
+}
+
+function deleteRecipeById(id) {
+  let recipeIndex = recipes.findIndex(recipe => recipe.id == id);
+  if (recipeIndex != -1) {
+    recipes.splice(recipeIndex, 1);
+    return { msg: `Recipe deleted: ${id}` };
+  }
+  else
+    return { errMsg: `Cannot find recipe by id: ${id}` }
+}
+
+function isParamEmpty(p) {
+  if (typeof (p) != 'string')
+    return true;
+  if (p.trim().length == 0)
+    return true;
+  return false;
+}
+
+function isValidImageUrl(imageUrl) {
+  const urlPattern = /^(https?:\/\/)?[\w.-]+\.[a-z]{2,}(\/\S*)?$/;
+  return urlPattern.test(imageUrl);
+}
+
+
+module.exports = {
+  seedSampleData,
+  getRecipes,
+  findRecipeById,
+  findRecipesByKeyword,
+  addRecipe,
+  deleteRecipeById
+};
